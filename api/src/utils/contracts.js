5e69f61eb6f8e7b6474524a6a0ac729c08f58fbb
patrick <blockchainbuddha@gmail.com>
2021-03-19 20:11:26 -0400

    Sweep out the old backend

--- api/src/utils/contracts.js
+++
@@ -1,9 +0,0 @@
-import niftyChessABI from './niftyChessAbi'
-
-const CONTRACTS = {
-  niftyChess: {
-    abi: niftyChessABI,
-    xdai: '0xAe7ca55Ce4511C848ac4F9C0F26abD9ecaaee2c6',
-  },
-}
-export default CONTRACTS
